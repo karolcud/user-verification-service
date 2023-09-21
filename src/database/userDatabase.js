b3f6551a4afd493cfc043c35f8fe3cc66dd6ba45
Karol Cudzich <karol_cudzich@discovery.com>
2023-09-21 11:56:32 +0200

    Added mock version of simply database with users

---
+++ src/database/userDatabase.js
@@ -0,0 +1,12 @@
+/* This is a mock version of database object storing information about users */
+
+export default [
+    {
+        login: 'Martita',
+        id: 'mm432_test79k93d'
+    },
+    {
+        login: 'Karolas',
+        id: 'kc135_79k93test5'
+    },
+];
