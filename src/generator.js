b1cbeaf090d36d39c47011c390d81cd06344228c
Karol Cudzich <kcudzich@core-logic.com>
2023-10-20 14:28:22 +0200

    dodanie średników po CodeRieviev

--- src/generator.js
+++ src/generator.js
@@ -1,13 +1,13 @@
 export function generateId() {
-    const stringAndNumber = '0123456789abcdefghijklmnoprstuwxyz'
-    let tab = []
+    const stringAndNumber = '0123456789abcdefghijklmnoprstuwxyz';
+    let tab = [];
     for (let i = 0; i < 16; i++) {
-        let strNumIndex = Math.floor(Math.random() * stringAndNumber.length)
-        let strNum = stringAndNumber.charAt(strNumIndex)
-        tab.push(strNum)
+        let strNumIndex = Math.floor(Math.random() * stringAndNumber.length);
+        let strNum = stringAndNumber.charAt(strNumIndex);
+        tab.push(strNum);
     }
 
-    tab[5] = '_'
-    let ciagStrNum = tab.join("")
+    tab[5] = '_';
+    let ciagStrNum = tab.join("");
     return ciagStrNum;
 }
